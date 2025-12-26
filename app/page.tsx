5af0c1faa888716591ef189c2ddbd60a65bbaedf
nothingx-x <sirmrmmr@gmail.com>
2025-12-26 20:43:11 +0330

    Add focus some animations

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <button></button>
       </section>
       <section className="flex flex-col gap-2 items-center justify-center bg-primary h-15 w-full rounded-md text-secondary">
-        <p className="text-sm">نیلوفر ام مکانی برای یافتن سرورهای ماینکرفت</p>
+        <p className="text-sm text-center">نیلوفر ام مکانی برای یافتن سرورهای ماینکرفت</p>
       </section>
       <span className="h-9 bg-primary rounded-md w-full"></span>
       <section className="flex flex-col gap-2 items-center justify-center bg-primary rounded-md text-secondary relative w-full h-65">
@@ -21,8 +21,8 @@ export default function Home() {
           <Image src="/niloofar.png" alt="Niloofar MC Logo" width={50} height={50} />
           <p className="text-base text-blue-100">نیلوفر ام سی</p>
           <div className="flex flex-row items-center justify-center gap-2">
-            <button className="px-4 py-2 bg-accent hover:bg-purple-700 transition-colors duration-300 border-2 border-purple-900 shadow-2xl shadow-accent text-blue-200 cursor-pointer">سرور ها</button>
-            <button className="px-4 py-2 bg-secondary hover:bg-blue-700 transition-colors duration-300 border-2 border-primary shadow-2xl shadow-secondary text-blue-200 cursor-pointer">ثبت سرور</button>
+            <button className="px-4 py-2 bg-accent hover:bg-purple-700 focus:bg-purple-700 transition-colors duration-300 border-2 border-purple-900 shadow-2xl shadow-accent text-blue-200 cursor-pointer">سرور ها</button>
+            <button className="px-4 py-2 bg-secondary hover:bg-blue-700 focus:bg-blue-700 transition-colors duration-300 border-2 border-primary shadow-2xl shadow-secondary text-blue-200 cursor-pointer">ثبت سرور</button>
           </div>
         </div>
       </section>
