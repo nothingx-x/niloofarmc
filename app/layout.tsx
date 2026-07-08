a3ce25bc4e13642d1dbb6f45f37353a4b93a03e8
nothingx-x <sirmrmmr@gmail.com>
2026-07-08 12:14:15 +0330

    Add footer with logo and copyright notice; enhance server section layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Vazirmatn } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Header } from "@/components/header";
+import Link from "next/link";
+import Logo from "@/public/niloofar.png";
+import Image from "next/image";
 
 const vazirMatn = Vazirmatn({
   variable: "--font-vazirmatn",
@@ -35,13 +38,32 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const currentYear = new Date().toLocaleDateString("fa-IR").split("/")[0]; // Get the current year in Persian calendar
   return (
     <html lang="fa-IR" dir="rtl">
       <body
-        className={`${vazirMatn.className} ${royeen.className} antialiased`}
+        className={`${vazirMatn.className} ${royeen.className} antialiased scroll-auto`}
       >
         <Header />
         <main className="max-w-[1200px] mx-auto px-4 py-2">{children}</main>
+        <footer className="w-full rounded-t-md bg-primary/70 px-4 py-2">
+          <div className="flex flex-col gap-4">
+            <Link href={"/"}>
+              <div className="flex flex-row gap-2 items-center">
+                <Image src={Logo} alt="لوگو" className="w-20 h-20" />
+                <h2>نیلوفر ام سی</h2>
+              </div>
+            </Link>
+            <span className="text-center text-primary/70 text-xs/7">
+              این وبسایت هیچ ارتباطی با Microsoft و یا Mojang ندارد.
+              <br />
+              ما یک وبسایت رایگان جهت معرفی سرور های فارسی ماینکرفت هستیم.
+            </span>
+            <span className="text-center text-primary/70 text-xs">
+              طراحی با ❤️ - {currentYear}. تمامی حقوق محفوظ است.
+            </span>
+          </div>
+        </footer>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -64,11 +64,13 @@ export default async function Home() {
 
       <div className="w-full h-0.5 bg-primary/40 my-8 rounded-md"></div>
 
-      <section className="flex flex-col justify-center items-center gap-4 mt-12">
-        <h2 className="text-2xl">محبوب ترین سرور ها</h2>
-        <p className="text-base text-center text-primary/70 text-balance">
-          سرور هایی که بیشترین بازیکن را دارند
-        </p>
+      <section className="flex flex-col justify-center items-center gap-4 mt-12 lg:grid lg:grid-cols-2">
+        <div className="flex flex-col justify-center items-center gap-2">
+          <h2 className="text-2xl">محبوب ترین سرور ها</h2>
+          <p className="text-base text-center text-primary/70 text-balance">
+            سرور هایی که بیشترین بازیکن را دارند
+          </p>
+        </div>
         {Array.from({ length: 4 }).map((_, i) => (
           <div
             key={i}
@@ -98,6 +100,54 @@ export default async function Home() {
             </button>
           </div>
         ))}
+      </section>
+
+      <section
+        id="servers"
+        className="flex flex-col items-center justify-center gap-2 my-12"
+      >
+        <h2 className="text-2xl">سرورها</h2>
+        <div className="w-full h-80 lg:h-[500px] rounded-md overflow-y-scroll flex flex-col gap-4 shadow-primary/20 shadow-2xl">
+          {Array.from({ length: 20 }).map((_, i) => (
+            <div
+              key={i}
+              className="flex flex-row items-center justify-between gap-4 px-4 py-4 shadow-primary/20 shadow-xs bg-white/80 rounded-md"
+            >
+              <div className="flex flex-row items-center gap-2">
+                <Image
+                  src={Logo}
+                  alt={`لوگو سرور ${i + 1}`}
+                  className="rounded-full"
+                  width={40}
+                  height={40}
+                />
+                <h4 className="text-base text-black ">
+                  سرور {new Intl.NumberFormat("fa-IR").format(i + 1)}
+                </h4>
+                <div className="w-4 h-4 rounded-full bg-accent mr-20 lg:mr-40 mb-2"></div>
+                <p className="text-xs text-primary/70">
+                  آنلاین:{" "}
+                  {new Intl.NumberFormat("fa-IR").format(
+                    Math.floor(Math.random() * 100)
+                  )}
+                </p>
+              </div>
+              <button className="px-2 py-1 bg-accent hover:bg-purple-700 focus:bg-purple-700 transition-colors duration-300 border-2 border-purple-900 text-blue-200 cursor-pointer">
+                مشاهده
+              </button>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-4 items-center justify-center my-12">
+        <div className="flex flex-col gap-4 items-center justify-center bg-accent/70 px-4 py-2 rounded-md shadow-accent/20 shadow-2xl h-40 w-full md:w-[500px]">
+          <h5>حمایت از ما</h5>
+          <p className="text-sm text-center text-accent">
+            برای حمایت ما لازم نیست کار خاصی بکنی! فقط این وبسایت رو به بقیه
+            معرفی کن
+          </p>
+        </div>
       </section>
     </div>
   );
